cbbc8e1d2a89979f8823b92bf5aa0e437534550d
Shimul Mahmud Khatami <2001037@iot.bdu.ac.bd>
2024-11-25 00:17:22 +0600

    Deleting Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Movies from './components/movies'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([
+    { id: 1, title: "Chennai Express", genre: "Comedy, Romance", stock: "Limited", rate: 4 },
+    { id: 2, title: "3 Idiots", genre: "Comedy, Romance", stock: "Limited", rate: 5 },
+    { id: 3, title: "Laggan", genre: "Social, Historical", stock: "Limited", rate: 8 },
+  ]);
 
-  return <table className="table">
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Genre</th>
-        <th>Stock</th>
-        <th>Rate</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Chennai Express</td>
-        <td>Comedy, Romance</td>
-        <td>Limited</td>
-        <td>4</td>
-      </tr>
-    </tbody>
-    <tbody>
-      <tr>
-        <td>Chennai Express</td>
-        <td>Comedy, Romance</td>
-        <td>Limited</td>
-        <td>4</td>
-      </tr>
-    </tbody>
-  </table>
+  const handleDelete = (id) => {
+    const updatedMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(updatedMovies);
+  };
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie.id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre}</td>
+            <td>{movie.stock}</td>
+            <td>{movie.rate}</td>
+            <td>
+              <button
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  border: "none",
+                  padding: "5px 10px",
+                  cursor: "pointer",
+                  borderRadius: "4px",
+                }}
+                onClick={() => handleDelete(movie.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default App
+export default App;
