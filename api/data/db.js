0f485fcfeb2a015a3b3609114ccc941d5e13f41b
Shimul Mahmud Khatami <2001037@iot.bdu.ac.bd>
2024-12-01 00:20:34 +0600

    Initializing User Models and Starting of Hashing password

--- api/data/db.js
+++ api/data/db.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
+        const conn = await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log("MongoDB connected...");
-    } catch (err) {
-        console.error(err.message);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+export default connectDB;
--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const connectDB = require("./data/db");
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import connectDB from "./data/db.js"; // Ensure the file extension is explicitly provided
+import authRoutes from "./routes/authRoutes.js"; // Ensure the file extension is explicitly provided
 
-dotenv.config();
+dotenv.config(); // Load environment variables
+
 const app = express();
 
-connectDB();
+connectDB(); // Connect to the database
 
-app.use(morgan("dev"));
+app.use(morgan("dev")); // Use Morgan for logging
 
+// Routes
+app.use("/api/v1/auth", authRoutes);
+
+// Test route
 app.get("/products", (req, res) => {
     res.send("Hello world");
 });
 
+// Set up the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
