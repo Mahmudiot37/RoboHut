7ccac5439e6dc4260543b06c4b4486eaae9e34d6
Shimul Mahmud Khatami <2001037@iot.bdu.ac.bd>
2024-12-01 00:30:46 +0600

    Packages updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
