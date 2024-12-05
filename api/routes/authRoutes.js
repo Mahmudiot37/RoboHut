61afa684d3b5dfb7174836b8bba18fba6e91bb00
Shimul Mahmud Khatami <2001037@iot.bdu.ac.bd>
2024-12-06 02:43:35 +0600

    protected middlewares

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { registerController, loginController } from "../controllers/authController.js"
+import { registerController, loginController, testController } from "../controllers/authController.js"
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 // router object
 const router = express.Router();
 
@@ -8,5 +9,8 @@ router.post('/register', registerController);
 
 // Login
 router.post("/login", loginController);
+
+// test route
+router.get('/test', requireSignIn, isAdmin, testController);
 
 export default router;
