b136a708ed69248dc7923e54eb20402e80a46366
Shimul Mahmud Khatami <2001037@iot.bdu.ac.bd>
2024-12-07 03:28:23 +0600

    Adding Components

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./footer.jsx";
+import Header from "./header.jsx";
+
+const Layout = ({ children }) =>{
+    return (
+        <div>
+           <Header />
+            <main style={{ minHeight: '80vh' }}>{children}</main>
+           <Footer />
+
+        </div>
+    )
+}
+
+export default Layout;
