0f9d47d8f6f071801351843c989f1601bcf4de00
Shimul Mahmud Khatami <2001037@iot.bdu.ac.bd>
2024-12-08 04:44:01 +0600

    Navigation menu and pages Design

--- src/pages/pagenotFound.jsx
+++ src/pages/pagenotFound.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import Layout from "../components/layout/layout.jsx";
+import { Link } from "react-router-dom";
+import Layout from "../components/layout/layout";
 
-const pageNotFound = () =>{
-    return (
-        <Layout>
-            <h1>Can't find the page</h1>
-        </Layout>
-    )
-}
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+        <Link to="/" className="pnf-btn">
+          Go Back
+        </Link>
+      </div>
+    </Layout>
+  );
+};
 
-export default pageNotFound;
+export default Pagenotfound;
